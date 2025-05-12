8efd67c0c8a5149bf20c038d2964fc897c0fde67
Ho-Da-Dak <decoding22@gmail.com>
2025-05-12 14:42:39 +0900

    init : create project

---
+++ src/main/java/com/ssafy/tripchat/TripchatApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.tripchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripchatApplication.class, args);
+	}
+
+}
