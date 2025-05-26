1053f4889c9793bf60745d5be600785704a33f5d
Ho-Da-Dak <decoding22@gmail.com>
2025-05-26 13:57:02 +0900

    feat: [#18] 예약 response DTO 정보추가

--- src/main/java/com/ssafy/tripchat/reservation/dto/response/ParkingReservationResponse.java
+++ src/main/java/com/ssafy/tripchat/reservation/dto/response/ParkingReservationResponse.java
@@ -2,22 +2,45 @@ package com.ssafy.tripchat.reservation.dto.response;
 
 import com.ssafy.tripchat.reservation.domain.ReservationPeriod;
 import com.ssafy.tripchat.reservation.domain.Reservations;
+import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class ParkingReservationResponse {
+    private int reservationId;
     private int parkingLotId;
     private String username;
     private ReservationPeriod reservationPeriod;
+    private LocalDateTime createdAt;
+    private String attractionName;
 
 
     public static ParkingReservationResponse from(Reservations reservations) {
         return ParkingReservationResponse.builder()
+                .reservationId(reservations.getId())
+                .attractionName(reservations.getParkingLot().getAttraction().getTitle())
                 .parkingLotId(reservations.getParkingLot().getId())
                 .username(reservations.getMember().getUsername())
                 .reservationPeriod(reservations.getReservationPeriod())
+                .createdAt(reservations.getCreatedAt())
                 .build();
+    }
+
+    public ParkingReservationResponse(
+            int reservationId,
+            int parkingLotId,
+            String username,
+            ReservationPeriod reservationPeriod,
+            LocalDateTime createdAt,
+            String attractionName
+    ) {
+        this.reservationId = reservationId;
+        this.parkingLotId = parkingLotId;
+        this.username = username;
+        this.reservationPeriod = reservationPeriod;
+        this.createdAt = createdAt;
+        this.attractionName = attractionName;
     }
 }
